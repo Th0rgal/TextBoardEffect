6e86e05690a06038914742f69470ddb449f92ed8
Thomas Marchand <thomas.marchand44@gmail.com>
2019-06-08 09:34:14 +0200

    code style improved

--- text_board_effect.py
+++ text_board_effect.py
@@ -5,8 +5,7 @@ height = int(input("Input an height (>=3 and %2==1): "))-3
 lines = "\n".join(["#" + " " * (width-2) + "#"] * int((height/2)))
 spaces_prefix = int((width-len(word))/2)-1
 
-print("#"*width)
-print(lines)
-print("#" + " " * spaces_prefix + word + " " * (width-spaces_prefix-len(word)-2) + "#")
-print(lines)
-print("#"*width)
+print("\n".join(
+    ["#"*width, lines, # 1 will print all first lines except the one with the word
+    "#" + " " * spaces_prefix + word + " " * (width-spaces_prefix-len(word)-2) + "#", # 2 will print the word centered
+    lines,"#"*width])) # 3 same as 1 reverted
